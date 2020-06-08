c05b69276ada9cbbcd2ad4ea8c32696f84f592cb
meter <772413635@qq.com>
2020-06-08 23:55:58 +0800

    ...

--- config/index.js
+++ config/index.js
@@ -50,7 +50,7 @@ module.exports = {
     // Paths
     assetsRoot: path.resolve(__dirname, '../dist'),
     assetsSubDirectory: 'static',
-    assetsPublicPath: '/yyjk/chart/',
+    assetsPublicPath: '/xslab/',
 
     /**
      * Source Maps
