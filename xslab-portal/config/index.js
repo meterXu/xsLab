eecde2ea28ca0dc2fd56187a642b8476aa885721
isaac <xuhg@dpark.com.cn>
2021-07-22 09:55:51 +0800

    ....

--- xslab-portal/config/index.js
+++ xslab-portal/config/index.js
@@ -9,7 +9,7 @@ module.exports = {
 
     // Paths
     assetsSubDirectory: 'static',
-    assetsPublicPath: '/',
+    assetsPublicPath: '/xslab/portal',
     proxyTable: {},
 
     // Various Dev Server settings
