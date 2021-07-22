048d50f910eb839bfd5acb00ad20dc2d247d5199
isaac <xuhg@dpark.com.cn>
2021-07-22 10:11:13 +0800

    ....

--- xslab-portal/src/config.js
+++ xslab-portal/src/config.js
@@ -1,16 +1,16 @@
 export default {
     url:{
-        xsclublogo: "http://7u.isaacxu.com/favicon_logo_white.png",
+        xsclublogo: "https://img.xdo.icu/favicon_logo_white.png",
         version:"https://img.shields.io/github/v/tag/meterxu/vue-draw-xs",
-        architecture:"https://img.isaacxu.com/architecture.png",
+        architecture:"https://img.xdo.icu/architecture.png",
         vuedrawxs:"https://www.npmjs.com/package/vue-draw-xs",
         xslab:"https://app.isaacxu.com/xslab/editor",
         someList:[
-            "https://img.isaacxu.com/xslab_01.png?imageView2/0/h/400",
-            "https://img.isaacxu.com/xslab_02.png?imageView2/0/h/400",
-            "https://img.isaacxu.com/xslab_03.png?imageView2/0/h/400",
-            "https://img.isaacxu.com/xslab_04.png?imageView2/0/h/400",
-            "https://img.isaacxu.com/xslab_05.png?imageView2/0/h/400",
+            "https://img.xdo.icu/xslab_01.png?imageView2/0/h/400",
+            "https://img.xdo.icu/xslab_02.png?imageView2/0/h/400",
+            "https://img.xdo.icu/xslab_03.png?imageView2/0/h/400",
+            "https://img.xdo.icu/xslab_04.png?imageView2/0/h/400",
+            "https://img.xdo.icu/xslab_05.png?imageView2/0/h/400",
         ]
     }
 }
