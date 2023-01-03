c0d8b0cefbc020c25ce027e29d76f5c0796ea375
isaac <xhgainxq@qq.com>
2023-01-03 21:59:21 +0800

    ...

--- xslab-rearend/src/config.js
+++ xslab-rearend/src/config.js
@@ -2,7 +2,7 @@ import dotenv from 'dotenv'
 import path from 'path'
 
 dotenv.config({
-    path: path.resolve(`.env.${process.env.NODE_ENV}`)
+    path: path.resolve(`.env.${process.env.NODE_ENV.trim()}`)
 })
 
 
